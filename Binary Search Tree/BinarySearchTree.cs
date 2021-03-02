df6eb07dad20b51a8ba33869fc877ec96f0e1001
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-02 14:28:20 -0800

    Preliminary member variables and constructors set up in node and tree classes.

--- Binary Search Tree/BinarySearchTree.cs
+++ Binary Search Tree/BinarySearchTree.cs
@@ -8,5 +8,32 @@ namespace Binary_Search_Tree
 {
     class BinarySearchTree
     {
+        public Node root;
+        public Node parent;
+        public Node childLeft;
+        public Node childRight;
+
+
+        //Ctor
+        public BinarySearchTree()
+        {
+            //assign incoming node's parents swap these two
+            //assign incoming node's root
+           
+        }
+
+        //methods
+
+        //make root if none
+
+        //identify tree location
+            //foreach node in line
+            //go left or right
+                //if(no more down line)
+                    //{decide left or right, assign number as left or right child, assign parent to child, then assign child to parent}
+
+        //assign parent to node
+            //assign childLeft or ChildRight to parent
+
     }
 }
--- Binary Search Tree/Node.cs
+++ Binary Search Tree/Node.cs
@@ -8,5 +8,22 @@ namespace Binary_Search_Tree
 {
     class Node
     {
+        public int number;
+        public Node childNodeLeft;
+        public Node childNodeRight;
+        public Node parentNode;
+        public Node rootNode;
+
+        public Node(int number)
+        {
+            this.number = number;
+            
+        }
+
+        //methods
+
+        //assign root
+        //assign parent
+        //assign children left or right
     }
 }
