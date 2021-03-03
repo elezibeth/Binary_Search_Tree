485ced0330bb541ae2885bf8739ac10789faabd7
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-02 17:45:03 -0800

    BST assigns only one root test passes.

--- BSTTest/UnitTest1.cs
+++ BSTTest/UnitTest1.cs
@@ -9,27 +9,45 @@ namespace BSTTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+       
         
-        public void TestMethod1()
-        {
-            
-        }
 
         [TestMethod]
         public void BSTAssignsARoot()
         {
             BinarySearchTree tree = new BinarySearchTree();
             Node node1 = new Node(3);
-            bool expected = true;
+            int expected = 3;
+            
 
             tree.Add(node1);
+            int actual = tree.root.number;
 
-            bool actual = tree.root.rootNode;
+            
 
             //assert
             Assert.AreEqual(expected, actual);
 
         }
+        [TestMethod]
+        public void BSTAssignsOnlyOneRoot()
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+            Node node1 = new Node(3);
+            Node node2 = new Node(5);
+            int expected = 3;
+
+
+            tree.Add(node1);
+            tree.Add(node2);
+            int actual = tree.root.number;
+
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
     }
 }
