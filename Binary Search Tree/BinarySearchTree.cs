b0ff13e594954e2a0bd5202a1c45711eb27d61d0
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-02 22:01:23 -0800

    removed unused variables in BST class.

--- Binary Search Tree/BinarySearchTree.cs
+++ Binary Search Tree/BinarySearchTree.cs
@@ -9,11 +9,11 @@ namespace Binary_Search_Tree
     public class BinarySearchTree
     {
         public Node root;
-        public Node parent;
-        public Node childLeft;
-        public Node childRight;
+        //public Node parent;
+        //public Node childLeft;
+        //public Node childRight;
         public List<Node> list;
-        public Node[,] nodeArray;
+        public Node[,] nodeArray; //members in list have an array position after being sorted into position
 
 
 
@@ -22,8 +22,6 @@ namespace Binary_Search_Tree
         public BinarySearchTree()
         {
             list = new List<Node>();
-            //assign incoming node's parents swap these two
-            //assign incoming node's root
             nodeArray = new Node[10, 10];
 
         }
@@ -45,16 +43,11 @@ namespace Binary_Search_Tree
             else
             {
                 list.Add(node);
-                //determine parent
-                Node atThisPoint = root;
-                DetermineParent(node, atThisPoint);
-                //locate parent Node;s address
-                //update with new child
+                Node rootCopy = root;
+                DetermineParent(node, rootCopy);
                 UpdateParent(node);
-                //assign node an address based on parent's address
                 AssignNodeAddress(node, node.parentNode);
-                //Add node to data array at index
-                AddNodeToArray(node);
+                nodeArray[node.xAddress, node.yAddress] = node;
 
             }
 
@@ -89,29 +82,17 @@ namespace Binary_Search_Tree
                 }
             }
         }
-        public void AddNodeToArray(Node node)
-        {
-            int x = node.xAddress;
-            int y = node.yAddress;
-            nodeArray[x, y] = node;
-        }
+   
         public void UpdateParent(Node node)
         {
-            
-            int evenOdd = node.xAddress % 2;
-            if(evenOdd != 1)
+            if(node.xAddress % 2 != 1)
             {
-                int x = node.parentNode.xAddress;
-                int y = node.parentNode.yAddress;
-                nodeArray[x, y].childNodeLeft = node;
+                nodeArray[node.parentNode.xAddress, node.parentNode.yAddress] = node;
             }
             else
             {
-                int x = node.parentNode.xAddress;
-                int y = node.parentNode.yAddress;
-                nodeArray[x, y].childNodeRight = node;
+                nodeArray[node.parentNode.xAddress, node.parentNode.yAddress] = node;
             }
-          
         }
 
         public void DetermineParent(Node node, Node comparingNode)
@@ -127,9 +108,6 @@ namespace Binary_Search_Tree
             {
                 node.parentNode = comparingNode;
                 comparingNode.childNodeRight = node;
-
-
-
             }
             if (node.number < comparingNode.number && comparingNode.childNodeLeft != null)//...
             {
@@ -156,9 +134,6 @@ namespace Binary_Search_Tree
                 {
                     node.parentNode = comparingNode;
                     comparingNode.childNodeRight = node;
-
-
-
                 }
                 if (node.number <= 5 && comparingNode.childNodeLeft != null)//...
                 {
@@ -169,8 +144,6 @@ namespace Binary_Search_Tree
                 {
                     node.parentNode = comparingNode;
                     comparingNode.childNodeLeft = node;
-
-
                 }
 
             }
@@ -184,14 +157,7 @@ namespace Binary_Search_Tree
        
         
 
-        //identify tree location
-            //foreach node in line
-            //go left or right
-                //if(no more down line)
-                    //{decide left or right, assign number as left or right child, assign parent to child, then assign child to parent}
-
-        //assign parent to node
-            //assign childLeft or ChildRight to parent
+       
 
     
 
